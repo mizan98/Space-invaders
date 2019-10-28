07aefedcdb29bd697aa510d49fca812498c31d11
Mizan <mizan_98@hotmail.co.uk>
2019-10-28 17:20:22 +0000

    javascript experimentation

--- space.js
+++ space.js
@@ -1,7 +1,15 @@
-let btnEnter = document.getElementById("btn-enter")
-let enter = document.querySelector("entry")
-let game = document.querySelector("game")
+let entry = document.getElementsByClassName("entry")
+let game = document.getElementsByClassName("game")
+entry.hidden = false;
+game.hidden = true;
 
-btnEnter.addEventListener ("click", () => {
-    
-})
+document.getElementById("btn-enter")
+// let canvas = document.getElementById("game-start")
+.addEventListener ("click", function() {
+    entry.hidden = true;
+    game.hidden = false;
+}, false);
+
+
+// let display = canvas.getContext("2d")
+
