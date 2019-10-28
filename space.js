6399dab0a5466f4e3ddd2ab60d107c95984131a0
Mizan <mizan_98@hotmail.co.uk>
2019-10-28 15:35:10 +0000

    CSS basics done and button event listener added

---
+++ space.js
@@ -0,0 +1,7 @@
+let btnEnter = document.getElementById("btn-enter")
+let enter = document.querySelector("entry")
+let game = document.querySelector("game")
+
+btnEnter.addEventListener ("click", () => {
+    
+})
